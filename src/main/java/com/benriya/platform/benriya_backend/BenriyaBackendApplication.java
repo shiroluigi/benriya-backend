18a958b020757ce84cec883d17e3eb235dea0094
Rohit Mondal <rohit.luiji3@gmail.com>
2026-04-27 11:30:02 +0000

    initial

---
+++ src/main/java/com/benriya/platform/benriya_backend/BenriyaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.benriya.platform.benriya_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenriyaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenriyaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benriya/platform/benriya_backend/BenriyaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benriya.platform.benriya_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenriyaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
